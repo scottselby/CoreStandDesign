af0661c29ad6e9e26efef2d8448072c87c427bd3
scottselby <sselby@fanfueled.com>
2014-11-12 02:20:55 -0600

    hopefully a fix

--- CoreStandDesign/CoreStandDesign/App_Start/IdentityConfig.cs
+++ CoreStandDesign/CoreStandDesign/App_Start/IdentityConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
+//using Microsoft.AspNet.Identity;
+//using Microsoft.AspNet.Identity.EntityFramework;
+//using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using CoreStandDesign.Models;
 
